b76481dff8a429795af7a40201db17e2453e1882
mschlechtinger <michael@schlechtinger.de>
2017-10-29 02:09:48 +0200

    add svg to button press #stillUglyAsHell

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 $(document).foundation();
 
-this.stage;
 var that = this;
 
 // Din A4 == 210 * 297
@@ -33,12 +32,18 @@ window.onload = function () {
 
 };
 
+/**
+ * create trackpieces menu
+ * @param trackPiece: current trackpiece from JSON
+ */
 createTrackPiece = function (trackPiece) {
 
+	// Clickable Box
 	var box = document.createElement("div");
 	box.className = "primary callout";
 	box.onclick = createBox;
 
+	//
 	var text = document.createElement("p");
 	text.textContent = trackPiece.name;
 
@@ -50,30 +55,29 @@ createTrackPiece = function (trackPiece) {
 
 createBox = function() {
 
-
 	// create an exampleBox
 	var layer = new Konva.Layer();
 	var rectX = that.stage.getWidth() / 2 - 50;
 	var rectY = that.stage.getHeight() / 2 - 25;
 
+	var imageObj = new Image();
+	imageObj.onload = function() {
 
-	var box = new Konva.Rect({
-		x: rectX,
-		y: rectY,
-		width: 100,
-		height: 50,
-		fill: '#00D2FF',
-		stroke: 'black',
-		strokeWidth: 4,
-		draggable: true
-	});
-	// add cursor styling
-	box.on('mouseover', function() {
-		document.body.style.cursor = 'pointer';
-	});
-	box.on('mouseout', function() {
-		document.body.style.cursor = 'default';
-	});
-	layer.add(box);
-	that.stage.add(layer);
+		var box = new Konva.Image({
+			x: rectX,
+			y: rectY,
+			image: imageObj,
+			draggable: true
+		});
+		// add cursor styling
+		box.on('mouseover', function () {
+			document.body.style.cursor = 'pointer';
+		});
+		box.on('mouseout', function () {
+			document.body.style.cursor = 'default';
+		});
+		layer.add(box);
+		that.stage.add(layer);
+	};
+	imageObj.src = 'resources/images/Pylonentor.svg';
 };
